6bc26b22b3337f10b88980abf3808e78d4fe1caa
Andrea Llanes <llanesandrea0@gmail.com>
2026-05-07 18:32:47 -0300

    Requerimiento 2: Eliminar un producto

--- index.js
+++ index.js
@@ -78,7 +78,31 @@ async function gestion_de_productos ( args = [] ) {
                 console.log ("Solicitud incompleta");
                 break;
             };
-    }
+        case "DELETE":
+            if (args[1].includes ("/") && args[1].includes ("products")) {
+                let id_prod = args[1].split("/");
+                try {
+                    const id = parseInt (id_prod[1]);
+                    const response = await fetch (`${url}/products/${id}`, {
+                        method: "DELETE"
+                    });
+                    if (!response.ok) {
+                        throw new Error ("Error en la solicitud");
+                        break;
+                    }
+                    const data = await response.json ();
+                    console.log ("Se elimino el siguiente articulo:")
+                    console.log (data);
+                    break;
+                }
+                catch (error) {
+                    console.log (error);
+                    break;
+                }
+            } else {
+                console.log ("Solicitud incorrecta");
+            };
+    };
 };
 
 gestion_de_productos (args);
