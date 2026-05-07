7b9b8290b6d97830a6a310d8b4f4c7f13f30650f
Andrea Llanes <llanesandrea0@gmail.com>
2026-05-07 18:01:19 -0300

    Requerimiento 2: Consultar un producto específico

--- index.js
+++ index.js
@@ -24,11 +24,29 @@ async function gestion_de_productos ( args = [] ) {
                     }
                     const data = await response.json ();
                     data.forEach (elemento => {
-                        console.table (elemento);
+                        console.log (elemento);
                     });
                     break;
                 } catch (error) {
                     console.log(error);
+                    break;
+                }
+            } else if (args[1].includes("/") && args[1].includes("products")) {
+                let id_prod = args[1].split("/");
+                try {
+                    const id = parseInt (id_prod[1]);
+                    const response = await fetch (`${url}/products/${id}`, { 
+                        method: "GET"
+                    })
+                    if (response.status != 200) {
+                        throw new Error ("Error en la solicitud");
+                        break;
+                    }
+                    const data = await response.json ();
+                    console.log (data);
+                    break;
+                }catch (error) {
+                    console.log (error);
                     break;
                 }
             }
